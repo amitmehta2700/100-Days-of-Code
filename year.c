55c63de82a32a42dd34c34397f0c3ad09ee320ff
amitmehta2700 <amitmehta.2700@gmail.com>
2025-08-27 00:27:45 +0530

    Add files via upload

---
+++ year.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+    int year;
+    printf(" Enter a Year: ");
+    scanf("%d",&year);
+    if(year%4==0)
+    {
+        if(year%100!=0)
+        {
+            printf("Leap Year");
+        }
+        else
+        {
+            if(year%400==0)
+            {
+                printf("Leap Year");
+            }
+            else
+            {
+                printf("Not a Leap Year");
+            }
+        }
+    }
+    else
+    {
+        printf("Not a Leap Year");
+    }
+    return 0;
+}
