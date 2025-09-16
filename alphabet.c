594d79abbeb836d865b68aa35d25845e68fcf85e
amitmehta2700 <amitmehta.2700@gmail.com>
2025-09-16 10:01:39 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char ch;
+
+    // Input a character
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Check character type
+    if (isupper(ch)) {
+        printf("The character '%c' is an Uppercase Alphabet.\n", ch);
+    }
+    else if (islower(ch)) {
+        printf("The character '%c' is a Lowercase Alphabet.\n", ch);
+    }
+    else if (isdigit(ch)) {
+        printf("The character '%c' is a Digit.\n", ch);
+    }
+    else {
+        printf("The character '%c' is a Special Character.\n", ch);
+    }
+
+    return 0;
+}
