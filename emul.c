e3d2b060fa51b688b1ad28215e4ae06a19e35184
amitmehta2700 <amitmehta.2700@gmail.com>
2025-09-19 13:35:40 +0530

    Add files via upload

---
+++ emul.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+int i,n,mul=1;
+printf("Enter the value of n: ");
+scanf("%d", &n);
+for(i=1;i<=n;i++)
+{
+    if(i%2==0)
+    {
+        mul=mul*i;
+    }
+}
+printf("Product of even numbers is %d\n", mul);
+return 0;
+}
