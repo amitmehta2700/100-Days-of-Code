8e67ecff84264458cf2ea0b0e9746aeb02fc1663
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-22 17:49:09 +0530

    Add files via upload

---
+++ sumno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int r,n,i,sum=0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while(n>0) {
+        r=n%10;
+        sum=sum+r;
+        n=n/10;
+    }
+    printf("Sum of digits is %d\n", sum);
+    return 0;
+    }
