16cc8657a6b325e2afb0abddf9615cfd6aa5f0cb
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-22 23:39:29 +0530

    Add files via upload

---
+++ series.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main () {
+   int n,i;
+   int sum=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(i=2;i<=n;i+2) {
+        sum=1+(2*i-1)/(2*n);
+    } 
+    printf("The sum of the series is: %d\n",sum);
+    return 0;
+}
