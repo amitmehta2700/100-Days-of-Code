b850163f2a5d27b8ab1ed6591005eeeddec68cd6
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-22 23:24:47 +0530

    Add files via upload

---
+++ strong.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0, fact;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        fact = 1;
+        for (int j = 1; j <= i; j++) {
+            fact *= j;
+        }
+        sum += fact;
+    }
+    if (sum == n) {
+        printf("%d is a strong number.\n", n);
+    } else {
+        printf("%d is not a strong number.\n", n);
+    }
+    return 0;
+}
