9d6bd437b80d89a677f537694f0e0fed623cf193
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-22 18:12:06 +0530

    Add files via upload

---
+++ complement.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    printf("Enter a number (1-7): ");
+    if (scanf("%d", &num) != 1) {
+        printf("Invalid input!\n");
+        return 1;
+    }
+
+    switch (num) {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2:
+            printf("Monday\n");
+            break;
+        case 3:
+            printf("Tuesday\n");
+            break;
+        case 4:
+            printf("Wednesday\n");
+            break;
+        case 5:
+            printf("Thursday\n");
+            break;
+        case 6:
+            printf("Friday\n");
+            break;
+        case 7:
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Invalid number! Please enter a number between 1 and 7.\n");
+            break;
+    }
+
+    return 0;
+}
