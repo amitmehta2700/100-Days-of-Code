0eeac18d806503af36073ff3aa7489c173023242
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-23 14:03:44 +0530

    Add files via upload

---
+++ onepnz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, -5, 0, 20, -15, 0, 7};
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    int positive_count = 0;
+    int negative_count = 0;
+    int zero_count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > 0) {
+            positive_count++;
+        } else if (arr[i] < 0) {
+            negative_count++;
+        } else { // arr[i] == 0
+            zero_count++;
+        }
+    }
+    printf("Number of positive elements: %d\n", positive_count);
+    printf("Number of negative elements: %d\n", negative_count);
+    printf("Number of zero elements: %d\n", zero_count);
+
+    return 0;
+}
