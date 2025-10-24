c5223636425807245e1b7a6646d8c31e6f15ee25
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-25 00:17:53 +0530

    Add files via upload

---
+++ onedel.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void deleteElement(int arr[], int *n, int key) {
+    int i = 0;
+    while (i < *n && arr[i] != key) {
+        i++;
+    }
+    if (i == *n) {
+        return;
+    }
+    for (int j = i; j < *n - 1; j++) {
+        arr[j] = arr[j + 1];
+    }
+    (*n)--;
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int keyToDelete = 30;
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    deleteElement(arr, &n, keyToDelete);
+
+    printf("Array after deleting %d: ", keyToDelete);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
