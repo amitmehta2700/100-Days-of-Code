b055509ccb7400c84e34f6013d80b6b7abf85591
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-25 00:35:32 +0530

    Add files via upload

---
+++ twom.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter the number of rows: ");
+    scanf("%d", &r);
+    printf("Enter the number of columns: ");
+    scanf("%d", &c);
+
+    int matrix[r][c];
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("Element [%d][%d]: ", i + 1, j + 1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    printf("\nThe matrix is:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
