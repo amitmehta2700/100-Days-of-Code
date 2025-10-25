6d3b7585ebb21aa707c6a423d373ab62929ae76d
amitmehta2700 <amitmehta.2700@gmail.com>
2025-10-25 23:20:12 +0530

    Add files via upload

---
+++ twodia.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, k, flag = 0;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+    printf("Enter elements of matrix:\n");
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(a[i][i] == a[j][j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if(flag) break;
+    }
+
+    if(flag == 0)
+        printf("All diagonal elements are distinct.\n");
+    else
+        printf("Diagonal elements are not distinct.\n");
+
+    return 0;
+}
