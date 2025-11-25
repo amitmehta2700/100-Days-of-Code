9e262c1f38285ee0bc48cc6230c0919644a66d8e
amitmehta2700 <amitmehta.2700@gmail.com>
2025-11-25 21:35:21 +0530

    Add files via upload

---
+++ Day51.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+int firstOccurrence(int nums[], int n, int target) {
+    int low = 0, high = n - 1, ans = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            ans = mid;          // possible answer
+            high = mid - 1;     // search in left half
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int lastOccurrence(int nums[], int n, int target) {
+    int low = 0, high = n - 1, ans = -1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] == target) {
+            ans = mid;          // possible answer
+            low = mid + 1;      // search in right half
+        }
+        else if (nums[mid] < target) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int n, target;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter sorted array elements: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    int first = firstOccurrence(nums, n, target);
+    int last = lastOccurrence(nums, n, target);
+
+    printf("First Occurrence: %d\n", first);
+    printf("Last Occurrence: %d\n", last);
+
+    return 0;
+}
