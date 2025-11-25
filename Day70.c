5ba00a068121a22cb52f3518f48bf6bf12a65023
amitmehta2700 <amitmehta.2700@gmail.com>
2025-11-26 00:34:41 +0530

    Add files via upload

---
+++ Day70.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[200];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    if (str[0] != '\0')
+        str[0] = toupper(str[0]);
+
+    for (int i = 1; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+
+    printf("Sentence case: %s", str);
+
+    return 0;
+}
